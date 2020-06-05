8bc5b0b47362632933a2eb55405f853b0aeaef6e
Leon Rösler <leon@MacBook-Pro.fritz.box>
2020-06-05 18:47:33 +0200

    refactor(sass files): rename sass files and imports to match conventions

---
+++ user-frontend/src/components/reusables/CategoryCheckbutton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CategoryCheckbutton() {
+    return (
+        <div className="category-checkbutton">
+            <p>
+                Text
+            </p>
+        </div>
+    )
+}
+
+export default CategoryCheckbutton
