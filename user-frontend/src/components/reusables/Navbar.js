a84db064c6c8703823147b38af0395c827d7cbde
Leon Rösler <leon@MacBook-Pro.fritz.box>
2020-06-09 15:59:09 +0200

    feat(navbar): move and resize nav-indicator onClick

--- user-frontend/src/components/reusables/Navbar.js
+++ user-frontend/src/components/reusables/Navbar.js
@@ -1,62 +1,108 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, createRef} from 'react'
 import HomeIcon from '@material-ui/icons/Home';
 import LiveTvIcon from '@material-ui/icons/LiveTv';
 import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
 import Videocam from '@material-ui/icons/Videocam';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
-
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     const [activeTab, setActiveTab] = useState('')
+    const [leftPosition, setLeftPosition] = useState('0px')
+    const [indicatorWidth, setIndicatorWidht] = useState('0px')
+
+    const activeRef = createRef();
+
+    // move Indicator every time the window resizes
+    useEffect (() => {
+        window.addEventListener("resize", moveIndicator())
+        console.log('move')
+        return () => window.removeEventListener("resize", moveIndicator())
+    })
+
+    // to move to active indicator
+    const moveIndicator = () => {
+        if(activeRef.current == null) return
+        setLeftPosition(activeRef.current.getBoundingClientRect().left + 'px')
+        setIndicatorWidht(activeRef.current.getBoundingClientRect().width + 'px')
+    }
     
     const getActiveRoute = (route, location, match) => {
         if (!match) {
             return false;
           }
         location.pathname === route ? setActiveTab(route) : setActiveTab('')  
+        moveIndicator()
     }
+
 
     return (
         <nav className="nav">
             <div className="main nav__flex-container">
                 <input className='nav__searchBar' type='text' name='suche' placeholder='Video, Playlist, Channel, Stichwort...'/>
                 <ul className='nav-ul'>
-                    <li className='nav__element'> 
-                        <NavLink to ='/' exact className={`nav-link${activeTab === '/' ? '--isActive' : ''}`} activeClassName="isActive" isActive={(match, location) => getActiveRoute('/', location, match)}>
+                    <li className='nav__element'
+                            ref={activeTab === '/' ? activeRef : null}> 
+                        <NavLink 
+                            to ='/' 
+                            exact 
+                            className={`nav-link${activeTab === '/' ? '--isActive' : ''}`} 
+                            activeClassName="isActive" 
+                            isActive={(match, location) => getActiveRoute('/', location, match)}
+                            >
                             <HomeIcon className="nav__icon"/>
                             Home
                         </NavLink>
-                        {activeTab === '/' && <div className='tabIndicator'/>}
                     </li>
-                    <li className='nav__element'> 
-                        <NavLink to ='/channel' exact className={`nav-link${activeTab === '/channel' ? '--isActive' : ''}`} activeClassName="isActive" isActive={(match, location) => getActiveRoute('/channel', location, match)}>
+                    <li className='nav__element' ref={activeTab === '/channel' ? activeRef : null}>
+                        <NavLink 
+                            to ='/channel' 
+                            exact 
+                            className={`nav-link${activeTab === '/channel' ? '--isActive' : ''}`} 
+                            activeClassName="isActive" 
+                            isActive={(match, location) => getActiveRoute('/channel', location, match)}
+                            >
                             <LiveTvIcon className="nav__icon"/>
                             Channels
                         </NavLink>
-                        {activeTab === '/channel' && <div className='tabIndicator'/>}
                     </li>
-                    <li className='nav__element'> 
-                        <NavLink to ='/playlist' exact className={`nav-link${activeTab === '/playlist' ? '--isActive' : ''}`} activeClassName="isActive" isActive={(match, location) => getActiveRoute('/playlist', location, match)}>
+                    <li className='nav__element' ref={activeTab === '/playlist' ? activeRef : null}>
+                        <NavLink 
+                            to ='/playlist' 
+                            exact 
+                            className={`nav-link${activeTab === '/playlist' ? '--isActive' : ''}`} 
+                            activeClassName="isActive" 
+                            isActive={(match, location) => getActiveRoute('/playlist', location, match)}
+                            >
                             <PlaylistPlayIcon className="nav__icon"/>
                             Playlists
                         </NavLink>
-                        {activeTab === '/playlist' && <div className='tabIndicator'/>}
                     </li>
-                    <li className='nav__element'> 
-                        <NavLink to ='/live' exact className={`nav-link${activeTab === '/live' ? '--isActive' : ''}`} activeClassName="isActive" isActive={(match, location) => getActiveRoute('/live', location, match)}>
+                    <li className='nav__element'  ref={activeTab === '/live' ? activeRef : null}>
+                        <NavLink 
+                            to ='/live' 
+                            exact 
+                            className={`nav-link${activeTab === '/live' ? '--isActive' : ''}`} 
+                            activeClassName="isActive" 
+                            isActive={(match, location) => getActiveRoute('/live', location, match)}
+                            >
                             <FiberManualRecordIcon className="nav__icon nav__icon--live"/>
                             Live
                         </NavLink>
-                        {activeTab === '/live' && <div className='tabIndicator'/>}
                     </li>
-                    <li className='nav__element'> 
-                        <NavLink to ='/video-services' exact className={`nav-link${activeTab === '/video-services' ? '--isActive' : ''}`} activeClassName="isActive" isActive={(match, location) => getActiveRoute('/video-services', location, match)}>
+                    <li className='nav__element' ref={activeTab === '/video-services' ? activeRef : null}>
+                        <NavLink 
+                            to ='/video-services'
+                            exact 
+                            className={`nav-link${activeTab === '/video-services' ? '--isActive' : ''}`} 
+                            activeClassName="isActive" 
+                            isActive={(match, location) => getActiveRoute('/video-services', location, match)}
+                            >
                             <Videocam className="nav__icon"/>
                             Video&nbsp;Services
                         </NavLink>
-                        {activeTab === '/video-services' && <div className='tabIndicator'/>}
                     </li>
+                    <span className="nav__indicator" style={{left: leftPosition, width: indicatorWidth}}></span>
                 </ul>
             </div>
         </nav>
