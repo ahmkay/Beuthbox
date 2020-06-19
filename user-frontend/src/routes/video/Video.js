c74533981aaee1804d799d09e488910143c9ab4f
MarlonMattern <mmn@dasburo.com>
2020-06-19 18:34:17 +0200

    feat(clipbord-copy): share button copies url to clipboard and displays snackbar

--- user-frontend/src/routes/video/Video.js
+++ user-frontend/src/routes/video/Video.js
@@ -1,140 +1,180 @@
-import React, {useState, useEffect, useLayoutEffect, useRef } from 'react'
-import axios from 'axios'
-import {BASEURL} from '../../api'
-import Moment from 'react-moment';
-import ColorLensIcon from '@material-ui/icons/ColorLens';
-import TodayIcon from '@material-ui/icons/Today';
-import ShareIcon from '@material-ui/icons/Share';
-import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
-import LiveTvIcon from '@material-ui/icons/LiveTv';
-import SecondaryButton from '../../components/reusables/SecondaryButton';
-import { Link } from 'react-router-dom'
-import VideoRow from '../../components/reusables/VideoRow';
-import CategoryIcon from '../../components/reusables/CategoryIcon'
-
+import React, { useState, useEffect, useLayoutEffect, useRef } from "react";
+import axios from "axios";
+import { BASEURL } from "../../api";
+import Moment from "react-moment";
+import TodayIcon from "@material-ui/icons/Today";
+import ShareIcon from "@material-ui/icons/Share";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import LiveTvIcon from "@material-ui/icons/LiveTv";
+import SecondaryButton from "../../components/reusables/SecondaryButton";
+import { Link } from "react-router-dom";
+import VideoRow from "../../components/reusables/VideoRow";
+import CategoryIcon from "../../components/reusables/CategoryIcon";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
 
 const Video = (props) => {
-    const [video, setVideo ] = useState([])
-    const [height, setHeight] = useState(1000)
-    const { id } = props.match.params
-    const videoContainerRef = useRef(null)
+  const [video, setVideo] = useState([]);
+  const [height, setHeight] = useState(1000);
+  const [copySuccess, setCopySuccess] = useState(false);
+  const { id } = props.match.params;
+  const videoContainerRef = useRef(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const video = await axios.get( `${BASEURL}/graphql?query={video(id:"${id}"){name, description, created, uploaded, playerType, tags, status, source, uploadedByUser, access, videoDuration, dualView, isOpencast, posterImagePath, videoPath, modified, _id, views}}`)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const video = await axios.get(
+          `${BASEURL}/graphql?query={video(id:"${id}"){name, description, created, uploaded, playerType, tags, status, source, uploadedByUser, access, videoDuration, dualView, isOpencast, posterImagePath, videoPath, modified, _id, views}}`
+        );
 
-                let videoPathString = video.data.data.video.videoPath.toString();
-                let string = videoPathString.replace("http://beuthbox-opencast.beuth-hochschule.de/static/mh_default_org/engage-player/", "");
-                let n = string.indexOf("/");
-                let OCid = string.slice(0, n);
-                video.data.data.video.ocid = OCid;
-                setVideo(video.data.data.video)
-            }
-            catch(error) { console.log(error)}
-        }
-        fetchData()
-        console.log(props, 'props')
-    }, [id])
+        let videoPathString = video.data.data.video.videoPath.toString();
+        let string = videoPathString.replace(
+          "http://beuthbox-opencast.beuth-hochschule.de/static/mh_default_org/engage-player/",
+          ""
+        );
+        let n = string.indexOf("/");
+        let OCid = string.slice(0, n);
+        video.data.data.video.ocid = OCid;
+        setVideo(video.data.data.video);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [id]);
 
-    const shareVideo = () => document.execCommand('copy');
-    
-    const showTags = () =>  video.tags.map((tag, index) => <Link to={`/search?tag=${tag}`} className={`video--tag ${index === 0 ? 'firstchild' : '' }`} ><h5>#{tag}</h5></Link>)
+  const shareVideo = () => {
+    const url = window.location.href;
+    let textArea = document.createElement("textarea");
+    textArea.value = url;
+    document.body.appendChild(textArea);
+    textArea.style = "none";
+    textArea.select();
+    document.execCommand("copy");
+    setCopySuccess(true);
+    document.body.removeChild(textArea);
+  };
 
-    console.log(video, 'video Object')
-    
-    const getHeight = () => {
-        setHeight(videoContainerRef.current.clientHeight - 240 + 'px')
-        console.log(height)
+
+  function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
     }
+    setCopySuccess(false);
+  };
 
-    useLayoutEffect(() => {
-        if ( videoContainerRef !== null && videoContainerRef.current !== null) {
-       window.addEventListener('resize', getHeight)
-        }
-        return () => window.removeEventListener('resize', getHeight)
-    })
-    console.log(video, 'videoData')
+  const showTags = () =>
+    video.tags.map((tag, index) => (
+      <Link
+        to={`/search?tag=${tag}`}
+        className={`video--tag ${index === 0 ? "firstchild" : ""}`}
+      >
+        <h5>#{tag}</h5>
+      </Link>
+    ));
 
-    if(video) {
-       return (
-           <>
-           <div className='root-container'>
-            <div className='video-content-container'>
-           <div className='container-65-left' style={{display: 'inline-block'}} ref={videoContainerRef}>
-               <div className='video-container'>
-                    <iframe allowfullscreen 
-                        src={`http://beuthbox-opencast.beuth-hochschule.de/paella/ui/embed.html?id=${video.ocid}`}
-                        style={{border: 'none', width: '100%', height: '70vh'}} 
-                        name="Paella Player" 
-                        scrolling="no" 
-                        frameborder="0" 
-                        marginheight="0px" 
-                        marginwidth="0px">
-                    </iframe>
-               </div>
-               <div className='video-info-container'>
-                <div className='video-category-container'>
-                    <PlaylistPlayIcon className='video-playlist--icon'/>
-                    <p className='video-playlist--headline'>
-                        Playlist:
-                    </p>
-                    <Link to='/' className='video--tag'>
-                        <h4 className='video-playlist--name'>
-                            PlaylistName
-                        </h4>
-                    </Link>
+  const getHeight = () => {
+    setHeight(videoContainerRef.current.clientHeight - 240 + "px");
+  };
 
-
-                    <LiveTvIcon className='video-livetv--icon'/>
-                    <p className='video-channel--headline'>
-                        Channel:
-                    </p>
-                    <Link to='/' className='video--tag'>
-                        <h4 className='video-channel--name'>
-                            ChannelName
-                        </h4>
-                    </Link>
-                    </div>
-                    <div className='video-createdAt-container'>
-                    <TodayIcon className='today--icon' />
-                    <small className='createdAt'>
-                        <Moment format="DD.MM.YY">{video.created}</Moment>
-                    </small>
-                </div>
-               </div>
-               <div className='video-info-container'>
-                <div className='video-category-container'>
-                    <CategoryIcon 
-                        type = 'label'
-                        category = 'study'
-                        isActive
-                    />
-                    {video.tags && showTags()}
-                </div>
-                
-               </div>
-               <h2 className='video-headline'>{video.name}</h2>
-               <p className='video-description'>
-                   {video.description}
-               </p>
-               <SecondaryButton text={'Teilen'} onClick={shareVideo} icon={ShareIcon} additionalClasses='share-button'></SecondaryButton>
-           </div>
-           <div className='video-playlist--container' style={{height}}>
-           <VideoRow videos={video} flexDirection="column" headline="Mehr aus der Playlist" />
-           </div>
-           </div>
-           
-           <VideoRow videos={video} amountOfVideos={4}  headline='Ähnliche Videos' flexDirection="row" />
-           </div>
-           </>
-       )
+  useLayoutEffect(() => {
+    if (videoContainerRef !== null && videoContainerRef.current !== null) {
+      window.addEventListener("resize", getHeight);
     }
+    return () => window.removeEventListener("resize", getHeight);
+  });
+
+  if (video) {
     return (
-        <div>
-            Video
-        </div>
-    )
-}
+      <>
+        <div className="root-container">
+          <div className="video-content-container">
+            <div
+              className="container-65-left"
+              style={{ display: "inline-block" }}
+              ref={videoContainerRef}
+            >
+              <div className="video-container">
+                <iframe
+                  allowfullscreen
+                  src={`http://beuthbox-opencast.beuth-hochschule.de/paella/ui/embed.html?id=${video.ocid}`}
+                  style={{ border: "none", width: "100%", height: "70vh" }}
+                  name="Paella Player"
+                  scrolling="no"
+                  frameborder="0"
+                  marginheight="0px"
+                  marginwidth="0px"
+                ></iframe>
+              </div>
+              <div className="video-info-container">
+                <div className="video-category-container">
+                  <PlaylistPlayIcon className="video-playlist--icon" />
+                  <p className="video-playlist--headline">Playlist:</p>
+                  <Link to="/" className="video--tag">
+                    <h4 className="video-playlist--name">PlaylistName</h4>
+                  </Link>
 
-export default Video
+                  <LiveTvIcon className="video-livetv--icon" />
+                  <p className="video-channel--headline">Channel:</p>
+                  <Link to="/" className="video--tag">
+                    <h4 className="video-channel--name">ChannelName</h4>
+                  </Link>
+                </div>
+                <div className="video-createdAt-container">
+                  <TodayIcon className="today--icon" />
+                  <small className="createdAt">
+                    <Moment format="DD.MM.YY">{video.created}</Moment>
+                  </small>
+                </div>
+              </div>
+              <div className="video-info-container">
+                <div className="video-category-container">
+                  <CategoryIcon type="label" category="study" isActive />
+                  {video.tags && showTags()}
+                </div>
+              </div>
+              <h2 className="video-headline">{video.name}</h2>
+              <p className="video-description">{video.description}</p>
+              <SecondaryButton
+                text={"Teilen"}
+                onClick={shareVideo}
+                icon={ShareIcon}
+                additionalClasses="share-button"
+              ></SecondaryButton>
+            </div>
+            <div className="video-playlist--container" style={{ height }}>
+              <VideoRow
+                videos={video}
+                flexDirection="column"
+                headline="Mehr aus der Playlist"
+              />
+            </div>
+          </div>
+
+          <VideoRow
+            videos={video}
+            amountOfVideos={4}
+            headline="Ähnliche Videos"
+            flexDirection="row"
+          />
+          <Snackbar
+            open={copySuccess}
+            autoHideDuration={2000}
+            onClose={handleClose}
+          >
+            <Alert onClose={handleClose} severity="success">
+              Das Video befindert sich in der Zwischenablage.
+            </Alert>
+          </Snackbar>
+        </div>
+      </>
+    );
+  }
+  return <div>Video</div>;
+};
+
+export default Video;
