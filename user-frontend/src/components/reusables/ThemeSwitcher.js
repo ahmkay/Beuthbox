90f617b6df87aa215c720f47794afe3121a52a14
Leon Rösler <leon@MacBook-Pro.fritz.box>
2020-06-29 11:32:24 +0200

    feat(theme-switch): add styles for theme-switcher button

--- user-frontend/src/components/reusables/ThemeSwitcher.js
+++ user-frontend/src/components/reusables/ThemeSwitcher.js
@@ -4,12 +4,12 @@ import Light from "@material-ui/icons/WbSunny";
 
 const ThemeSwitcher = () => {
 
-    const [checked, setChecked] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches)
+    const [isDarkTheme, setIsDarkTheme] = useState(window.localStorage.getItem("theme") === 'dark')
 
     const handleSwitch = () => {
-        setChecked(!checked)
+        setIsDarkTheme(!isDarkTheme)
 
-        if(checked !== true) {
+        if(isDarkTheme !== true) {
             localStorage.setItem('theme', 'dark')
             document.documentElement.setAttribute('theme', 'dark')
         } else {
@@ -18,17 +18,13 @@ const ThemeSwitcher = () => {
         }
       };
 
-    const setIcon = () => {
-        if(checked) return {Night}
-        return {Light}
-    }
     return (
         <div
-            className={`theme-switcher ${!checked ? 'theme-switcher--dark' : 'theme-switcher--light'}`}
+            className={`theme-switcher ${!isDarkTheme ? 'theme-switcher--dark' : 'theme-switcher--light'}`}
             onClick={() => handleSwitch()}
         >
-            {!checked ? <Night className="theme-switcher__icon"/> : <Light className="theme-switcher__icon"/>}
-            <p className="theme-switcher__label">{!checked ? 'Dark Mode' : 'Light Mode'}</p>
+            {!isDarkTheme ? <Night className="theme-switcher__icon"/> : <Light className="theme-switcher__icon"/>}
+            <small className="theme-switcher__label">{isDarkTheme ? 'Light' : 'Dark'}</small>
         </div>
     )
 }
