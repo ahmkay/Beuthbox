96871d0a00033777981566329e87cb6cbeb81f7a
Leon Rösler <leon@MacBook-Pro.fritz.box>
2020-07-02 18:49:17 +0200

    feat(video-post): finish post-video styles and disable tab-option if there is no content

--- user-frontend/src/routes/channel/Channel.js
+++ user-frontend/src/routes/channel/Channel.js
@@ -3,14 +3,16 @@ import axios from "axios";
 import { BASEURL } from "../../api";
 import PlaylistsCarousel from "../../components/reusables/PlaylistsCarousel";
 import ChannelHeader from "./ChannelHeader";
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import SpeakerNotes from '@material-ui/icons/SpeakerNotes'
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import SpeakerNotes from "@material-ui/icons/SpeakerNotes";
 import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
 import Videocam from "@material-ui/icons/Videocam";
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import PostVideo from "./PostVideo";
+import NoContent from "../../components/reusables/NoContent";
 
 const Channel = (props) => {
   const [channel, setChannel] = useState([]);
@@ -29,7 +31,7 @@ const Channel = (props) => {
           `${BASEURL}/graphql?query={channelVideoCount(id: "${props.match.params.id}"){_id, total}}`
         );
         const responseVideos = await axios.get(
-          `${BASEURL}/graphql?query={videos(filter: {channelid: "${props.match.params.id}"}){_id, name, posterImagePath, created, status, access, views, videoDuration, categories{name, description, created, imagepath, iconpath _id}}}`
+          `${BASEURL}/graphql?query={videos(filter: {channelid: "${props.match.params.id}"}){_id, name, posterImagePath, created, status, access, views, videoDuration, description, categories{name, description, created, imagepath, iconpath _id}}}`
         );
 
         const responsecategories = await axios.get(
@@ -86,46 +88,55 @@ const Channel = (props) => {
   }, []);
 
   const showVideos = () => {
-    console.log(video, "videos");
+    return video
+      .sort((video1, video2) => video1.videoDuration - video2.videoDuration)
+      .map((video) => {
+        return (
+          <PostVideo
+            id={video._id}
+            img={`${BASEURL}/videos${video.posterImagePath}`}
+            title={video.name}
+            created={video.created}
+            description={video.description}
+            duration={video.videoDuration}
+          />
 
-    return video.map((video, index) => {
-      return (
-        <div class="item col-md-3 col-xs-12">
-          <div class="tile">
-            <a href={`video/${video._id}`}>
-              {video.posterImagePath.indexOf("engage-player") > 1 ? (
-                <img class="tile-image" src={video.posterImagePath} />
-              ) : (
-                <img
-                  class="tile-image"
-                  src={`${BASEURL}/videos${video.posterImagePath}`}
-                />
-              )}
-            </a>
-            <div class="tile-info">
-              <a href={`/video/${video._id}`}>
-                <div class="title">{video.name}</div>
-                <div class="details">
-                  <div class="created">
-                    <span class="glyphicon glyphicon-calendar"></span>
-                    {video.created}
-                  </div>
-                  <div class="time">
-                    <span class="glyphicon glyphicon-time"></span>
-                    {video.videoDuration}
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-        </div>
-      );
-    });
+          // <div class="item col-md-3 col-xs-12">
+          //   <div class="tile">
+          //     <a href={`video/${video._id}`}>
+          //       {video.posterImagePath.indexOf("engage-player") > 1 ? (
+          //         <img class="tile-image" src={video.posterImagePath} />
+          //       ) : (
+          //         <img
+          //           class="tile-image"
+          //           src={`${BASEURL}/videos${video.posterImagePath}`}
+          //         />
+          //       )}
+          //     </a>
+          //     <div class="tile-info">
+          //       <a href={`/video/${video._id}`}>
+          //         <div class="title">{video.name}</div>
+          //         <div class="details">
+          //           <div class="created">
+          //             <span class="glyphicon glyphicon-calendar"></span>
+          //             {video.created}
+          //           </div>
+          //           <div class="time">
+          //             <span class="glyphicon glyphicon-time"></span>
+          //             {video.videoDuration}
+          //           </div>
+          //         </div>
+          //       </a>
+          //     </div>
+          //   </div>
+          // </div>
+        );
+      });
   };
 
   function TabPanel(props) {
     const { children, value, index, ...other } = props;
-  
+
     return (
       <div
         role="tabpanel"
@@ -136,71 +147,75 @@ const Channel = (props) => {
       >
         {value === index && (
           <Box p={3}>
-            <Typography>{children}</Typography>
+            <Typography component={"span"} variant={"body2"}>
+              {children}
+            </Typography>
           </Box>
         )}
       </div>
     );
   }
 
-  const renderTabcontroller = () =>{
-
+  const renderTabcontroller = () => {
     const handleChange = (e, newValue) => {
       setValue(newValue);
     };
 
-      return (
-        <div className="channel-tabcontroller">
-          <AppBar position="static" color="default">
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              centered
-            >
-              <Tab label="Chronik" icon={<SpeakerNotes />} />
-              <Tab label="Playlists" icon={<PlaylistPlayIcon />} />
-              <Tab label="Videos" icon={<Videocam />} />
-            </Tabs>
-          </AppBar>
-          <TabPanel value={value} index={0}>
-            Item One
-          </TabPanel>
-          <TabPanel value={value} index={1}>
-            Item Two
-          </TabPanel>
-          <TabPanel value={value} index={2}>
-            {video.length != 0 && (
-                    <>
-                    <h3>Videos</h3>
-                    </>
-                )}
-                {showVideos()}
-          </TabPanel>
-        </div>
-    )
-  }
+    return (
+      <div className="channel-tabcontroller">
+        <AppBar position="static" color="default">
+          <Tabs value={value} onChange={handleChange} centered>
+            <Tab label="Chronik" icon={<SpeakerNotes />} />
+            <Tab
+              label="Playlists"
+              icon={<PlaylistPlayIcon />}
+              disabled={categories.length <= 0}
+            />
+            <Tab
+              label="Videos"
+              icon={<Videocam />}
+              disabled={video.length <= 0}
+            />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          {video.length <= 0 && <NoContent content="video" />}
+          {showVideos()}
+        </TabPanel>
+      </div>
+    );
+  };
 
   if (video && categories && numberOfVideos && channel) {
     return (
       <>
-        <ChannelHeader title={channel.name} description={channel.description} img={`${BASEURL}/channel${channel.imagepath}`}/>
-        {renderTabcontroller() }
+        <ChannelHeader
+          title={channel.name}
+          description={channel.description}
+          img={`${BASEURL}/channel${channel.imagepath}`}
+        />
+        {renderTabcontroller()}
         <main className="main">
-            <div class="container-fluid">
-                <div class="row player-container content">
-                </div>
-                <div class="row">
-                <div class="col-sm-12 channel-info">
-                    <div class="video-info"></div>
-                </div>
-                </div>
-
-                <div class="container-fluid">
-                {/* {categories.length != 0 && <h3 class="carousel-title">Playlist</h3>} */}
-                <PlaylistsCarousel playlists={categories} />
-                {/* <div class="row">{showCategories()}</div> */}
-                </div>
+          <div class="container-fluid">
+            <div class="row player-container content"></div>
+            <div class="row">
+              <div class="col-sm-12 channel-info">
+                <div class="video-info"></div>
+              </div>
             </div>
+
+            <div class="container-fluid">
+              {/* {categories.length != 0 && <h3 class="carousel-title">Playlist</h3>} */}
+              <PlaylistsCarousel playlists={categories} />
+              {/* <div class="row">{showCategories()}</div> */}
+            </div>
+          </div>
         </main>
       </>
     );
