8a9ccc95f84cc150ee20a27fd6a75b3248b3e70c
Leon Roesler <le.on@outlook.com>
2020-07-10 20:20:22 +0200

    style(channel-header): make channel-header responsive

--- user-frontend/src/routes/channel/ChannelHeader.js
+++ user-frontend/src/routes/channel/ChannelHeader.js
@@ -1,19 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function ChannelHeader({title, img, description}) {
-    return (
-        <header className="channel-header">
-            <img src={img} alt="Header Image" className="channel-header__img"/>
-            <div className="channel-header__overlay">
-                <div className="channel-header__content">
-                    <div className="channel-header__title-box">
-                        <h1 className="channel-header__title">{title}</h1>
-                    </div>
-                    <h4 className="channel-header__description">{description}</h4>
-                </div>
-            </div>
-        </header>
-    )
+function ChannelHeader({ title, img, description }) {
+  return (
+    <header className="channel-header">
+      <div className="channel-header__header-img">
+        <img src={img} alt="Header Image" className="channel-header__img" />
+        <span className="channel-header__overlay"></span>
+        <div className="channel-header__title-box">
+          <h1 className="channel-header__title">{title}</h1>
+        </div>
+      </div>
+      <h4 className="channel-header__description">{description}</h4>
+    </header>
+  );
 }
 
-export default ChannelHeader
+export default ChannelHeader;
