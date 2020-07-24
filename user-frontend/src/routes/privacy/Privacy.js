5ee381bf7d903508743feb95268e704f64e4e270
Leon Roesler <le.on@outlook.com>
2020-07-24 13:36:13 +0200

    feat(privacy): add privacy page

---
+++ user-frontend/src/routes/privacy/Privacy.js
@@ -0,0 +1,141 @@
+import React from "react";
+
+const Privacy = () => {
+  return (
+    <main className="main">
+      <header>
+        <h1 className="page-headline">Datenschutzerklärung</h1>
+      </header>
+      <section className="main__section">
+        <p>
+          Diese Datenschutzerklärung klärt Sie über die Art, den Umfang und
+          Zweck der Verarbeitung von personenbezogenen Daten (nachfolgend kurz
+          „Daten“) innerhalb unseres Onlineangebotes und der mit ihm verbundenen
+          Webseiten, Funktionen und Inhalte sowie externen Onlinepräsenzen auf
+          (nachfolgend gemeinsam bezeichnet als „Onlineangebot“). Im Hinblick
+          auf die verwendeten Begrifflichkeiten, wie z.B. „Verarbeitung“ oder
+          „Verantwortlicher“ verweisen wir auf die Definitionen im Art. 4 der
+          Datenschutzgrundverordnung (DSGVO).
+        </p>
+        <h5>Verantwortlicher</h5>
+        <p>
+          Verantwortlich für die Webseite:
+          <br />
+          Prof. Dr. Robert Strzebkowski <br />
+          FBVI Informatik und Medien <br />
+          Laborleiter Film und Fernsehen <br />
+          Srobert.strzebkowski@beuth-hochschule.de
+        </p>
+        <h3>Maßgebliche Rechtsgrundlagen</h3>
+        <p>
+          Nach Maßgabe des Art. 13 DSGVO teilen wir Ihnen die Rechtsgrundlagen
+          unserer Datenverarbeitungen mit. Sofern die Rechtsgrundlage in der
+          Datenschutzerklärung nicht genannt wird, gilt Folgendes: Die
+          Rechtsgrundlage für die Einholung von Einwilligungen ist Art. 6 Abs. 1
+          lit. a und Art. 7 DSGVO, die Rechtsgrundlage für die Verarbeitung zur
+          Erfüllung unserer Leistungen und Durchführung vertraglicher Maßnahmen
+          sowie Beantwortung von Anfragen ist Art. 6 Abs. 1 lit. b DSGVO, die
+          Rechtsgrundlage für die Verarbeitung zur Erfüllung unserer rechtlichen
+          Verpflichtungen ist Art. 6 Abs. 1 lit. c DSGVO, und die
+          Rechtsgrundlage für die Verarbeitung zur Wahrung unserer berechtigten
+          Interessen ist Art. 6 Abs. 1 lit. f DSGVO. Für den Fall, dass
+          lebenswichtige Interessen der betroffenen Person oder einer anderen
+          natürlichen Person eine Verarbeitung personenbezogener Daten
+          erforderlich machen, dient Art. 6 Abs. 1 lit. d DSGVO als
+          Rechtsgrundlage.
+        </p>
+        <h3>Google Analytics</h3>
+        <p>
+          Diese Website nutzt Funktionen des Webanalysedienstes Google
+          Analytics. Anbieter ist die Google Inc., 1600 Amphitheatre Parkway
+          Mountain View, CA 94043, USA. <br />
+          Google Analytics verwendet so genannte "Cookies". Das sind
+          Textdateien, die auf Ihrem Computer gespeichert werden und die eine
+          Analyse der Benutzung der Website durch Sie ermöglichen. Die durch den
+          Cookie erzeugten Informationen über Ihre Benutzung dieser Website
+          werden in der Regel an einen Server von Google in den USA übertragen
+          und dort gespeichert.
+        </p>
+        <h5>IP Anonymisierung</h5>
+        <p>
+          Wir haben auf dieser Website die Funktion IP-Anonymisierung aktiviert.
+          Dadurch wird Ihre IP-Adresse von Google innerhalb von Mitgliedstaaten
+          der Europäischen Union oder in anderen Vertragsstaaten des Abkommens
+          über den Europäischen Wirtschaftsraum vor der Übermittlung in die USA
+          gekürzt. Nur in Ausnahmefällen wird die volle IP-Adresse an einen
+          Server von Google in den USA übertragen und dort gekürzt. Im Auftrag
+          des Betreibers dieser Website wird Google diese Informationen
+          benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die
+          Websiteaktivitäten zusammenzustellen und um weitere mit der
+          Websitenutzung und der Internetnutzung verbundene Dienstleistungen
+          gegenüber dem Websitebetreiber zu erbringen. Die im Rahmen von Google
+          Analytics von Ihrem Browser übermittelte IP-Adresse wird nicht mit
+          anderen Daten von Google zusammengeführt.
+        </p>
+        <h5>Browser Plugin</h5>
+        <p>
+          Sie können die Speicherung der Cookies durch eine entsprechende
+          Einstellung Ihrer Browser-Software verhindern; wir weisen Sie jedoch
+          darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche
+          Funktionen dieser Website vollumfänglich werden nutzen können. Sie
+          können darüber hinaus die Erfassung der durch den Cookie erzeugten und
+          auf Ihre Nutzung der Website bezogenen Daten (inkl. Ihrer IP-Adresse)
+          an Google sowie die Verarbeitung dieser Daten durch Google verhindern,
+          indem Sie das unter dem folgenden Link verfügbare Browser-Plugin
+          herunterladen und installieren:{" "}
+          <a
+            href="https://tools.google.com/dlpage/gaoptout?hl=de"
+            target="_blank"
+          >
+            https://tools.google.com/dlpage/gaoptout?hl=de
+          </a>
+          .
+        </p>
+        <h5>Widerspruch gegen Datenerfassung</h5>
+        <p>
+          Sie können die Erfassung Ihrer Daten durch Google Analytics
+          verhindern, indem Sie auf folgenden Link klicken. Es wird ein
+          Opt-Out-Cookie gesetzt, der die Erfassung Ihrer Daten bei zukünftigen
+          Besuchen dieser Website verhindert: Google Analytics deaktivieren
+        </p>
+        <p>
+          Mehr Informationen zum Umgang mit Nutzerdaten bei Google Analytics
+          finden Sie in der Datenschutzerklärung von Google:{" "}
+          <a
+            href="https://support.google.com/analytics/answer/6004245?hl=de"
+            target="_blank"
+          >
+            https://support.google.com/analytics/answer/6004245?hl=de
+          </a>
+          .
+        </p>
+        <h5>Auftragsdatenverarbeitung</h5>
+        <p>
+          Wir haben mit Google einen Vertrag zur Auftragsdatenverarbeitung
+          abgeschlossen und setzen die strengen Vorgaben der deutschen
+          Datenschutzbehörden bei der Nutzung von Google Analytics vollständig
+          um.
+        </p>
+        <h3>Google Tag Manager</h3>
+        <p>
+          Google Tag Manager ist eine Lösung, mit der wir sog. Website-Tags über
+          eine Oberfläche verwalten können (und so z.B. Google Analytics sowie
+          andere Google-Marketing-Dienste in unser Onlineangebot einbinden). Der
+          Tag Manager selbst (welches die Tags implementiert) verarbeitet keine
+          personenbezogenen Daten der Nutzer. Im Hinblick auf die Verarbeitung
+          der personenbezogenen Daten der Nutzer wird auf die folgenden Angaben
+          zu den Google-Diensten verwiesen. Nutzungsrichtlinien:{" "}
+          <a
+            href="https://www.google.com/intl/de/tagmanager/use-policy.html"
+            target="_blank"
+          >
+            https://www.google.com/intl/de/tagmanager/use-policy.html
+          </a>{" "}
+          .
+        </p>
+      </section>
+    </main>
+  );
+};
+
+export default Privacy;
