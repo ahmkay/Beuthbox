ef2b2386cd1820e95e5f60e1aa98fe9d003009cb
leonroesler <le.on@outlook.com>
2021-01-10 16:21:48 +0100

    feat(video-services): add video services grid

---
+++ user-frontend/src/routes/video-services/ServiceCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function ServiceCard({ imgName, title, description }) {
+  let image = null;
+
+  // get img
+  try {
+    image = require("../../assets/img/video-services/" + imgName + ".svg");
+  } catch (error) {}
+
+  return (
+    <article className="service-card paper--elevation-1">
+      {image && (
+        <img src={image} alt={`${title}-img`} className="service-card__img" />
+      )}
+      <h4 className="service-card__title">{title}</h4>
+      <h5 className="service-card__description">{description}</h5>
+    </article>
+  );
+}
--- user-frontend/src/routes/video-services/VideoServices.js
+++ user-frontend/src/routes/video-services/VideoServices.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Illustration from "../../assets/img/Illustration_Cameraman.svg";
+import ServiceCard from "./ServiceCard";
+import Services from "./Services.json";
 
 const VideoServices = () => {
+  const renderServiceCards = () => {
+    return Services.map((service) => {
+      return (
+        <ServiceCard
+          imgName={service.img}
+          title={service.title}
+          description="Durchführung von Lehrfilmaufnahmen im neuen Lehrfilmstudio im Haus Bauwesen, Raum D-103"
+        />
+      );
+    });
+  };
+
   return (
     <main className="main">
       <header className="vs-header">
         <h1 className="page-headline">Video Services</h1>
-        <div className="vs-header__subline">
+        <section className="vs-header__subline">
           <img
             src={Illustration}
             alt="video services illustration"
@@ -20,10 +34,13 @@ const VideoServices = () => {
             weiterbilden oder euren Vortrag Live streamen? Wir unterstützen euch
             dabei!
           </h3>
-        </div>
+        </section>
       </header>
       <section className="main__section main__section--vs">
-        <h2>Welche Services bietet das beuthBOX Team?</h2>
+        <h2 className="main__section__headline">
+          Welche Services bietet das beuthBOX Team?
+        </h2>
+        <div className="video-services__grid">{renderServiceCards()}</div>
       </section>
     </main>
   );
